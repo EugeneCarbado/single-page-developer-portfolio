d78381cfbb4d60ec4defa8d8f20f0445d6093354
Eugene carbado <50775092+EugeneCarbado@users.noreply.github.com>
2023-03-16 08:43:26 +0000

    Addeding tablet media queries

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import {socialLinks} from '../../data/links';
 import ProfileImageMobile from '../../images/image-profile-mobile.webp';
+import ProfileImageTablet from '../../images/image-profile-tablet.webp';
+import ProfileImageDesktop from '../../images/image-profile-desktop.webp';
 import PatternRings from '../../images/pattern-rings.svg';
 import './Hero.css';
 
 const Hero = () => {
+  const windowWidth = window.innerWidth;
+  const tabletMedia = 768;
+  const desktopMedia = 1024;
+
   return (
     <section className="bg-portfolio-black pb-10">
-      {/* <div className="grid grid-cols-3"> */}
-      <div className="container">
-        <div className="circleImage"></div>
-        <img className="portfolioImage z-10" src={ProfileImageMobile} />
-        <img className="elongCicleImage" src={PatternRings} />
-        <nav className="mx-auto w-[154px] pb-3 flex flex-col justify-center items-center navLinks z-20">
+      <div className="container relative">
+        <div className="circle-image"></div>
+        <img
+          className="portfolio-image z-10"
+          src={
+            windowWidth < tabletMedia ? ProfileImageMobile : ProfileImageTablet
+          }
+        />
+        <img
+          className="elongated-circle-image absolute left-[-118px] top-[2px] w-[530px] h-[129px]"
+          src={PatternRings}
+        />
+        <nav className="mx-auto w-[154px] pb-3 flex flex-col justify-center items-center nav-links z-20">
           <p className="text-white text-2xl font-bold">adamkeyes</p>
           <div className="pt-5 flex flex-row w-full justify-between">
             {socialLinks.map((item, key) => (
@@ -31,30 +44,7 @@ const Hero = () => {
           </div>
         </nav>
       </div>
-      {/* <div className="container">
-        <div className="circleImage"></div>
-        <img className="portfolioImage z-10" src={ProfileImageMobile} />
-        <img className="elongCicleImage" src={PatternRings} />
-        <nav className="mx-auto w-[154px] pb-3 flex flex-col justify-center items-center navLinks z-20">
-          <p className="text-white text-2xl font-bold">adamkeyes</p>
-          <div className="pt-5 flex flex-row w-full justify-between">
-            {socialLinks.map((item, key) => (
-              <a
-                href={item.hrefLink}
-                target="_blank"
-                rel="noreferrer"
-                key={key}>
-                <img
-                  className={item.imageStyles}
-                  src={item.imageLink}
-                  alt={item.altTag}
-                />
-              </a>
-            ))}
-          </div>
-        </nav>
-      </div> */}
-      <div className="pt-10 flex flex-col items-center">
+      <div className="pt-10 flex flex-col items-center md:hidden">
         <div className="text-[40px] leading-10 text-white text-center font-bold">
           <p>Nice to meet you!</p>
           <p>
@@ -65,7 +55,7 @@ const Hero = () => {
             .
           </p>
         </div>
-        <div className="pt-6 text-base leading-6 text-portfolio-grey text-center font-medium">
+        <div className="pt-8 text-base leading-6 text-portfolio-grey text-center font-medium">
           <p>Based in the UK, I&apos;m a front-end developer</p>
           <p>passionate about building accessible web</p>
           <p>apps that users love.</p>
